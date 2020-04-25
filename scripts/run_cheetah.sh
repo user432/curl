3cd989bcadee7857e965649312644caf9c8b6627
user432 <girishcricket432@gmail.com>
2020-04-25 21:53:24 +0530

    cheetah and cartpole

---
+++ scripts/run_cheetah.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    --domain_name cheetah \
+    --task_name run \
+    --encoder_type pixel \
+    --action_repeat 8 \
+    --save_tb --pre_transform_image_size 100 --image_size 84 \
+    --work_dir ./ \
+    --agent curl_sac --frame_stack 3 \
+    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000 \
+    --save_video
